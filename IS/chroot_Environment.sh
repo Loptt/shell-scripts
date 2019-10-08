c7ffe22891fef201892d0f762b9def0630cd65eb
vsapiens <erick.frank@icloud.com>
2019-10-08 18:29:39 -0500

    Added environment with tail -n 1

--- IS/chroot_Environment.sh
+++ IS/chroot_Environment.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo "Establishing virtual environment for apache ..."
+
 mkdir -p /var/www/var/run
+
 chown -R root:root /var/www/var/run
+
 s="ChrootDir /var/www"
-n=$(cat /etc/apache2/apache2.conf | grep PidFile -n | cut -d: -f1)
+n=$(cat /etc/apache2/apache2.conf | grep PidFile -n | cut -d: -f1|tail -n 1)
+
 sed -i "$n a $s" /etc/apache2/apache2.conf
-exit
